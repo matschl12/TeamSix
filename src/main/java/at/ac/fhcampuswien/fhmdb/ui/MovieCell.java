9a4f6d079cbcab6b2069f5aa050b7a02a1e083b3
matschl12 <matschlgg@gmail.com>
2023-04-04 23:43:57 +0200

    Now the genres of the movie are shown below the movie description

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -8,12 +8,17 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 
 public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
-    private final VBox layout = new VBox(title, detail);
-    private final Label genres = new Label(); // idk if it works
+    private final Label genres = new Label(); //to show genres
+
+    private final VBox layout = new VBox(title, detail, genres);
+
+    Font genreFont = Font.font("Verdana", FontPosture.ITALIC, 10); //to make genre font italic
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -29,18 +34,23 @@ public class MovieCell extends ListCell<Movie> {
                     movie.getDescription() != null
                             ? movie.getDescription()
                             : "No description available");
-            genres.setText(movie.getGenres().toString()); // idk if it works
+            genres.setText(movie.getGenres().toString().replace("[", "").replace("]", "")); //to get informations to show genres
+
 
 
             // color scheme
             title.getStyleClass().add("text-yellow");
             detail.getStyleClass().add("text-white");
+            genres.getStyleClass().add("text-white"); //added
+            genres.setFont(genreFont); //added
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
             title.fontProperty().set(title.getFont().font(20));
             detail.setMaxWidth(this.getScene().getWidth() - 30);
             detail.setWrapText(true);
+            genres.setMaxWidth(this.getScene().getWidth() - 30); //added
+            genres.setWrapText(true); //added
             layout.setPadding(new Insets(10));
             layout.spacingProperty().set(10);
             layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
