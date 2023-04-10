a6f2b0f0b70b3a51da01924583ce604120f2bfa2
matschl12 <matschlgg@gmail.com>
2023-04-10 02:58:07 +0200

    Made some goofy ass changes

--- src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
@@ -3,7 +3,9 @@ package at.ac.fhcampuswien.fhmdb;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 import java.util.Objects;
@@ -12,9 +14,10 @@ public class FhmdbApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FhmdbApplication.class.getResource("home-view.fxml"));
+        stage.getIcons().add(new Image(FhmdbApplication.class.getResourceAsStream("TeamSixIcon.png")));
         Scene scene = new Scene(fxmlLoader.load(), 890, 620);
         scene.getStylesheets().add(Objects.requireNonNull(FhmdbApplication.class.getResource("styles.css")).toExternalForm());
-        stage.setTitle("FHMDb");
+        stage.setTitle("TeamSix-FHMDb");
         stage.setScene(scene);
         stage.show();
     }
