0003693a81acedc679fd37c9a8143f86eea88f20
LeDanse <113842990+bitparcour@users.noreply.github.com>
2023-05-11 05:57:07 +0200

    debugging. sometimes it works. other times not.

--- src/main/java/at/ac/fhcampuswien/fhmdb/Exceptions/AllExceptionsHandler.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/Exceptions/AllExceptionsHandler.java
@@ -10,12 +10,12 @@ public class AllExceptionsHandler extends RuntimeException  {
 //this will catch and handle all exceptions also beyond the 2 custom exception classes
 
 
-/*    public AllExceptionsHandler(String message, SQLException e){
+    public AllExceptionsHandler(String message){
         super(message);
         DialoguesAndMessages.alertBox("An exception occurred","A built-in exception occurred", message);
 
     }
-*/
+
     public AllExceptionsHandler(String headerText, String contextMessage) {
         super(contextMessage);
 
@@ -29,39 +29,38 @@ public class AllExceptionsHandler extends RuntimeException  {
     }
 
     /*
-    public AllExceptionsHandler(String message){
-        super(message);
-        DialoguesAndMessages.alertBox("An exception occurred","A built-in exception occurred", message);
+         public AllExceptionsHandler(String message){
+             super(message);
+             DialoguesAndMessages.alertBox("An exception occurred","A built-in exception occurred", message);
 
-    }
+         }
 
-    public static int catchExceptions() {
-        return 0;
-    }
-/*
-    public static void forAnyOtherExceptions(){
-        System.out.println("nothing");
+         public static int catchExceptions() {
+             return 0;
+         }
+     /*
+         public static void forAnyOtherExceptions(){
+             System.out.println("nothing");
 
-    Thread thread = new Thread(() -> {
-        try {
-            // whatever
-        } catch (Exception e) {
+         Thread thread = new Thread(() -> {
+             try {
+                 // whatever
+             } catch (Exception e) {
 
-            myCustomMessagesTextArea.setText(e.getMessage());
+                 myCustomMessagesTextArea.setText(e.getMessage());
 
 
 
-        });
-        thread.setUncaughtExceptionHandler((t, e) -> {
-            myCustomMessagesTextArea.setText(e.getMessage());
-        });
-        thread.start();
+             });
+             thread.setUncaughtExceptionHandler((t, e) -> {
+                 myCustomMessagesTextArea.setText(e.getMessage());
+             });
+             thread.start();
 
-        Scene scene = new Scene(root, 300, 200);
-        stage.setScene(scene);
-        stage.show();
-
-*/
-    }
+             Scene scene = new Scene(root, 300, 200);
+             stage.setScene(scene);
+             stage.show();
+    */
+}
 
 
